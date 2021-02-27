72ee518df7ea3a2da1aa1b40e77bba1544948a50
Nikita <30903256+NikitosGolubev@users.noreply.github.com>
2021-02-27 20:57:00 +0300

    Set up vuetify

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import i18n from './i18n';
+import App from '@/App.vue';
+import router from '@/router';
+import store from '@/store';
+import i18n from '@/i18n';
+import vuetify from '@/plugins/vuetify';
 
 Vue.config.productionTip = false;
 
 new Vue({
+  vuetify,
   i18n,
   router,
   store,
