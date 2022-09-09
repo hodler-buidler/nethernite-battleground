cb776c02de13e52134672953e324a57902b446f6
Hodler-Buidler <30903256+NikitosGolubev@users.noreply.github.com>
2022-09-09 21:08:22 +0300

    Fix deploy

--- src/config/general.js
+++ src/config/general.js
@@ -1,5 +1,5 @@
 import { LOCAL_STORAGE_TYPE } from './storage/types';
 
 export const DEFAULT_STORAGE_TYPE = LOCAL_STORAGE_TYPE;
-export const BRAND_NAME = 'NikitosGolubev';
-export const AUTHOR_GITHUB_ACCOUNT = 'https://github.com/NikitosGolubev';
+export const BRAND_NAME = 'HodlerBuidler';
+export const AUTHOR_GITHUB_ACCOUNT = 'https://github.com/hodler-buidler';
